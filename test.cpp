84496ce359d51aae72b8e70ae060ac89633a9df8
Rohit Kumar Sadhukhan <rohit.sadhukhan1234@gmail.com>
2022-03-31 22:22:48 +0530

    file reading and comment part remove done

---
+++ test.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+  
+int main(int argc, char** argv)
+{
+    vector<string> query;
+    
+    
+    if(argc==1){
+        cout<< "Enter no of the query" << endl;
+        int count;
+        cin >> count;
+        for(int i=0;i<count;i++){
+            //cout <<"hii";
+            string s;
+            getchar();
+            getline(cin, s);
+            int found = s.find("--");
+            if (found != string::npos){
+                s.erase(s.begin() + found, s.end());
+            }
+            query.push_back(s);
+		}
+        cout << query[0];
+    }
+    else if(argc==2){
+        // Creation of ifstream class object to read the file
+        ifstream fin;
+        string line;
+        // by default open mode = ios::in mode
+        fin.open(argv[1]);
+       
+        
+        
+        // Execute a loop until EOF (End of File)
+        while (fin) {
+    
+            // Read a Line from File
+            getline(fin, line);
+            //cout<<line;
+            int found = line.find("--");
+            if (found != string::npos){
+                line.erase(line.begin() + found, line.end());
+            }
+            if(line!="")
+                query.push_back(line);
+            //cout<<line<<endl;
+        }
+        
+        // Close the file
+        fin.close();
+
+    }
+    return 0;
+}
---
+++ testsql.sql
@@ -0,0 +1,2 @@
+select * from abc;	--comment
+select name from stud where roll='2';
